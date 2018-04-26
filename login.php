40939e6822d773d90d87d89b6d7a9d67f2afb519
Christian C <christianc@ATM0560738DYN.dhcp.missouri.edu>
2018-04-26 09:47:15 -0500

    changes

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>EasyOrg - Login</title>
+        <link rel="stylesheet" type="text/css" href="login.css">
+    </head>
+    
+    <body>
+    
+    </body>
+
+</html>
